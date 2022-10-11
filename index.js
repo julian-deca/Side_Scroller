96bd8364a1d09b97f01e8759b00cfad12e6052ee
julian <juliandecastelli@gmail.com>
2022-10-11 20:17:33 -0300

    add jumping

--- index.js
+++ index.js
@@ -42,6 +42,8 @@ window.addEventListener("load", () => {
       this.frameX = 0;
       this.frameY = 1;
       this.speed = 0;
+      this.vy = 0;
+      this.weight = 1;
     }
     update(input) {
       this.x += this.speed;
@@ -49,12 +51,25 @@ window.addEventListener("load", () => {
         this.speed = 5;
       } else if (input.keys.indexOf("ArrowLeft") > -1) {
         this.speed = -5;
+      } else if (input.keys.indexOf("ArrowUp") > -1 && this.onGround()) {
+        this.vy -= 32;
       } else {
         this.speed = 0;
       }
       if (this.x < 0) this.x = 0;
       else if (this.x > this.gameWidth - this.width)
         this.x = this.gameWidth - this.width;
+
+      this.y += this.vy;
+      if (!this.onGround()) {
+        this.vy += this.weight;
+        this.frameY = 1;
+      } else {
+        this.vy = 0;
+        this.frameY = 0;
+      }
+      if (this.y > this.gameHeight - this.height)
+        this.y = this.gameHeight - this.height;
     }
     draw(context) {
       context.fillStyle = "white";
@@ -70,6 +85,9 @@ window.addEventListener("load", () => {
         this.width,
         this.height
       );
+    }
+    onGround() {
+      return this.y >= this.gameHeight - this.height;
     }
   }
   class Enemy {}
