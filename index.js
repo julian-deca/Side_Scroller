6fbc7da625ec806de202b9611a4d955864195d6d
julian <juliandecastelli@gmail.com>
2022-10-11 19:26:04 -0300

    add basic player class

--- index.js
+++ index.js
@@ -4,12 +4,73 @@ window.addEventListener("load", () => {
   const WIDTH = (canvas.width = 800);
   const HEIGHT = (canvas.height = 720);
 
-  class InputHandlers {}
-  class Player {}
+  class InputHandlers {
+    constructor() {
+      this.keys = [];
+      window.addEventListener("keydown", (e) => {
+        if (
+          (e.key === "ArrowDown" ||
+            e.key === "ArrowUp" ||
+            e.key === "ArrowRight" ||
+            e.key === "ArrowLeft") &&
+          this.keys.indexOf(e.key) === -1
+        ) {
+          this.keys.push(e.key);
+        }
+      });
+      window.addEventListener("keyup", (e) => {
+        if (
+          e.key === "ArrowDown" ||
+          e.key === "ArrowUp" ||
+          e.key === "ArrowRight" ||
+          e.key === "ArrowLeft"
+        ) {
+          this.keys.splice(this.keys.indexOf(e.key), 1);
+        }
+      });
+    }
+  }
+  class Player {
+    constructor(gameWidth, gameHeight) {
+      this.gameHeight = gameHeight;
+      this.gameWidth = gameWidth;
+      this.width = 200;
+      this.height = 200;
+      this.x = 0;
+      this.y = this.gameHeight - this.height;
+      this.image = document.getElementById("player");
+      this.frameX = 0;
+      this.frameY = 1;
+    }
+    update() {
+      this.x++;
+    }
+    draw(context) {
+      context.fillStyle = "white";
+      context.fillRect(this.x, this.y, this.width, this.height);
+      context.drawImage(
+        this.image,
+        this.width * this.frameX,
+        this.frameY * this.height,
+        this.width,
+        this.height,
+        this.x,
+        this.y,
+        this.width,
+        this.height
+      );
+    }
+  }
   class Enemy {}
   function handleEnemies() {}
   function displayStatusText() {}
+  const input = new InputHandlers();
+  const player = new Player(WIDTH, HEIGHT);
   function animate() {
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+    player.draw(ctx);
+    player.update();
     requestAnimationFrame(animate);
   }
+  animate();
 });
