e494350835aeca2c4b47603ad9ad91452e0cf396
julian <juliandecastelli@gmail.com>
2022-10-11 18:28:18 -0300

    add main structure

---
+++ index.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", () => {
+  const canvas = document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d");
+  const WIDTH = (canvas.width = 800);
+  const HEIGHT = (canvas.height = 720);
+
+  class InputHandlers {}
+  class Player {}
+  class Enemy {}
+  function handleEnemies() {}
+  function displayStatusText() {}
+  function animate() {
+    requestAnimationFrame(animate);
+  }
+});
